85ea62babfafd1dd31a31fe8b17f77ac579bd0e9
Ljuben Vassilev <ljuben.vassilev@gmail.com>
2018-07-22 22:20:14 +0300

    camera following

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public Transform player;
+    public float distance = 10f;
+    public float height = 5f;
+    public float cameraSpeed = 10f;
+    public float rotationSpeed = 10f;
+    public Vector3 lookOffset = new Vector3(0, 1, 0);
+
+    void FixedUpdate()
+    {
+        if (player)
+        {
+            Vector3 lookPosition = player.position + this.lookOffset;
+            Vector3 relativePosition = lookPosition - this.transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePosition);
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, rotation,
+                Time.deltaTime * this.rotationSpeed * 0.1f);
+            Vector3 targetPosition = player.transform.position + player.transform.up * this.height -
+                                     player.transform.forward * this.distance;
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition,
+                Time.deltaTime * this.cameraSpeed * 0.1f);
+            
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,26 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rigidBody;
-	public float speed = 10f;
-	public float rotationSpeed = 70f;
+	public float speed;
+	public float rotationSpeed;
         
 	void Start()
 	{
-		this.rigidBody = this.GetComponent<Rigidbody>();
+		rigidBody = this.GetComponent<Rigidbody>();
 	}
 
-	void LateUpdate()
+	private void FixedUpdate()
 	{
-		float translation = Input.GetAxis("Vertical") * Time.deltaTime * this.speed;
-		float rotation = Input.GetAxis("Horizontal") * Time.deltaTime * this.rotationSpeed;
-
-		Quaternion turnAngle = Quaternion.Euler(0f, rotation, 0f);
-
-		this.rigidBody.MovePosition(this.rigidBody.position + this.transform.forward * translation);
-		this.rigidBody.MoveRotation(this.rigidBody.rotation * turnAngle);
+		rigidBody.MovePosition(rigidBody.position + transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed);
+		rigidBody.MoveRotation(rigidBody.rotation * Quaternion.Euler(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0f));
 	}
 }
