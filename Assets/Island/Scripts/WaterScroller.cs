dd6cf870c7bd865d38e00219236ef5777b2331b8
Ljuben Vassilev <ljuben.vassilev@gmail.com>
2018-08-03 15:00:01 +0300

    waves particles complete

--- Assets/Island/Scripts/WaterScroller.cs
+++ Assets/Island/Scripts/WaterScroller.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterScroller : MonoBehaviour {
-	
+public class WaterScroller : MonoBehaviour 
+{	
 	public float scrollSpeed = 0.1f;
 	public Camera cam;
+	private Material mat;
 	private Renderer renderer;
 
 	private void Start()
 	{
 		renderer = GetComponent<Renderer>();
+		mat = renderer.sharedMaterial;
+	}
+	
+	void Update () {
 		if(renderer.material.shader.isSupported)
 			cam.depthTextureMode |= DepthTextureMode.Depth;
-	}
-
-	void Update () 
-	{
 		float offset = Time.time * scrollSpeed;
 		Vector2 k = new Vector2(offset / 10.0f, offset);
-		renderer.sharedMaterial.SetTextureOffset ("_MainTex", k);
+		mat.SetTextureOffset ("_MainTex", k);
 	}
 }
